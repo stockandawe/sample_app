7b50695e619c7feb70620f61e47d4eb76d2d6cb5
Rutul Dave <rutul98@yahoo.com>
2012-05-04 15:36:50 -0700

    1. Added a 'contact' page using TDD 2. Use guard to automate running of rspec tests 3. Speed up rspec tests with spork

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,11 +1,13 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+  let(:base_title) {"Ruby on Rails Tutorial Sample App"}
+  
   describe "Home page" do
 
-    it "show have the right title" do
+    it "should have the right title" do
       visit '/static_pages/home'
-      page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Home")
+      page.should have_selector('title', :text =>  "#{base_title} | Home")
     end
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
@@ -15,9 +17,9 @@ describe "StaticPages" do
 
   describe "Help page" do
 
-    it "show have the right title" do
+    it "should have the right title" do
       visit '/static_pages/help'
-      page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Help")
+      page.should have_selector('title', :text => "#{base_title} | Help")
     end
     it "should have the content 'Help'" do
       visit '/static_pages/help'
@@ -27,14 +29,26 @@ describe "StaticPages" do
   
   describe "About page" do
 
-    it "show have the right title" do
+    it "should have the right title" do
       visit '/static_pages/about'
-      page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | About Us")
+      page.should have_selector('title', :text => "#{base_title} | About Us")
     end    
     it "should have the content 'About Us'" do
       visit '/static_pages/about'
       page.should have_selector('h1', :text => 'About Us')
     end 
+  end
+  
+  describe "Contact" do
+    
+    it "shold have the right title" do
+      visit '/static_pages/contact'
+      page.should have_selector('title', :text => "#{base_title} | Contact")
+    end
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('h1', :text => 'Contact')
+    end
   end
 
 end
